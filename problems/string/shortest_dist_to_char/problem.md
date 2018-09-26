4aef7a0186e563d57995c626ab72fe664a9aa47c
alec cuccia <ajcuccia@gmail.com>
2018-09-26 14:45:03 -0400

    Add shortest_dist_to_char problem, explanation, and solution.js

---
+++ problems/string/shortest_dist_to_char/problem.md
@@ -0,0 +1,13 @@
+# Shortest Distance to a Character
+Given a string `str` and a character `char`, return an array of integers representing the shortest distance from the character `char` in the string.
+
+### Example 1:
+```
+Input: str = "loveleetcode", char = 'e'
+Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+```
+
+Note:
+* `str` length is between `[1, 10000]`.
+* `char` is a single character, and guaranteed to be in `str`.
+All letters in `str` and `char` are lowercase.
---
+++ problems/string/shortest_dist_to_char/solution.js
@@ -0,0 +1,18 @@
+var shortestToChar = function(str, char) {
+  let charIndices = [];
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] === char) charIndices.push(i);
+  };
+
+  let distances = [];
+  for(let i = 0; i < str.length; i++) {
+    let shortest = str.length;
+    for(let j = 0; j < charIndices.length; j++) {
+      let distance = Math.abs(i - charIndices[j]);
+      if(distance < shortest) shortest = distance;
+    };
+    distances.push(shortest);
+  };
+
+  return distances;
+};
